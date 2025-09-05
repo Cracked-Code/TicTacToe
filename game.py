c3c2b63b02b43058ce8066f274d7e0b0afd9423c
Nick <80059246+Cracked-Code@users.noreply.github.com>
2025-09-04 20:44:44 -0400

    Update game.py with bot

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from random import randint
+import time
 class TicTacToe :
     def __init__(self):
         self.reset()
@@ -6,22 +8,29 @@ class TicTacToe :
         self.board = [[' ' for _ in range(3)] for _ in range(3)]
         self.current_player = 'X'
         self.winner = None
+        self.player = True
     
     def get_state(self):
+        print(self.current_player)
         return {
             'board': self.board,
             'current_player': self.current_player,
-            'winner': self.winner
+            'winner': self.winner,
+            'turn' : self.whoturn()
         }
+    
     
 
     def make_move(self, row, col):
-        if self.winner is not None:
+        if self.winner:
             raise ValueError("Game is over")
         
         if self.board[row][col] == ' ':
-            self.board[row][col] = self.current_player
-            self.current_player = 'O' if self.current_player == 'X' else 'X'
+            self.board[row][col] = "X"
+            self.current_player = 'O'
+            self.turn()
+            
+            
         else:
             raise ValueError("Cell is already occupied")
         
@@ -52,8 +61,34 @@ class TicTacToe :
                 return self.winner
             else :
                 continue
+
+    def turn(self) :
+        if self.player == True:
+            self.player = False
+        else :
+            self.player = True
+
+    def whoturn(self) :
+        return self.player
         
-        return None
+    def ai_move(self) :
+        if self.player == True :
+            return
+        print("THIS IS AI MOVE")
+        self.current_player = 'X'
+        time.sleep(2)
+        while True :
+            xint = randint(0,2)
+            yint = randint(0,2)
+
+            if self.board[xint][yint] == ' ' :
+                self.board[xint][yint] = 'O'
+                self.turn()
+                break
+        if self.won():
+            self.winner = self.won()
+        
+    
 
 # game1 = TicTacToe()    
 # game1.board = [['X', ' ', 'X'], ['X', ' ', ' '], [' ', ' ', ' ']]
